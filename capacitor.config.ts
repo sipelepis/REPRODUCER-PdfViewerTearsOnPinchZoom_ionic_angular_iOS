22ec615660b11d20ec9fcef9c1c9a7661239a544
sipelepis <moran.phillipjan@ue.edu.ph>
2024-02-17 23:45:59 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PdfViewerTearsOnPinchZoom_ionic_angular_iOS',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
